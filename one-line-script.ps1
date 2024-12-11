a7c654ea59b602a08960b36a22a967761ab7b75f
Luca Pisano <lucapisano@microsoft.com>
2024-12-11 15:10:56 +0100

    parameters moved to head

--- one-line-script.ps1
+++ one-line-script.ps1
@@ -1,2 +1,2 @@
 #one-line SA attestation. Replace SubscriptionId at arg1 and TenantId at arg2
-$C=(New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/lucapisano/arc_sa_benefit/refs/heads/main/script.ps1");icm -ScriptBlock ([Scriptblock]::Create($c)) -ArgumentList @("SubscriptionId", "TenantId")
+$tenantId="xxx";$subId="xxx";$C=(New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/lucapisano/arc_sa_benefit/refs/heads/main/script.ps1");icm -ScriptBlock ([Scriptblock]::Create($c)) -ArgumentList @($subId, $tenantId)
